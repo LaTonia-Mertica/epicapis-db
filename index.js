378c7ed95aab59197e9d1a50b7e5820c662a264f
Sheppard Walker <latoniamertica@gmail.com>
2023-01-22 15:38:32 -0500

    email validation added

--- index.js
+++ index.js
@@ -32,6 +32,18 @@ server.get("/", (req, res) => {
 });
 
 server.post("/sendEmail", (req, res) => {
+  const email = req.body.email.trim();
+  const emailRegex =
+    /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
+  const regexResults = email.match(emailRegex);
+  const validEmail =
+    regexResults?.[0] === email && regexResults?.[0].length === email.length;
+
+  if (!validEmail) {
+    return res.send({ error: "Email Address Not Valid" });
+  }
+
   let html = `<i>Thank you for visiting</i>&nbsp;<strong>EPIC APIS</strong>.&nbsp;<i>Selections submitted are below</i>:<br>`;
 
   if (req.body.selections.beautifulEntry) {
